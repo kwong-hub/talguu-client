8f93912a4eecf0b74f6438809db2dacb90e57f7a
FIRST_NAME LAST_NAME <silogecho@gmail.com>
2021-03-19 16:38:51 +0300

    changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-let environment = "http://192.168.0.252:8080/api";
+let environment = "http://192.168.0.252:8081/api";
 let liveVideoURL = "http://8mspbb.com/"
 // let liveVideoURL = "http://192.168.0.105:8080";
 module.exports = {
